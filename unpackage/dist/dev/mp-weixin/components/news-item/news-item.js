fa71a7f1badbff5bec776e996e501dd12b8e9091
axx_monsey <liu75776@gmail.com>
2021-01-12 21:22:59 +0800

    merge goodsInfo

---
+++ unpackage/dist/dev/mp-weixin/components/news-item/news-item.js
@@ -0,0 +1,31 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["components/news-item/news-item"],{
+
+/***/ 91:
+/*!*****************************************************************************************************************!*\
+  !*** /Users/huangzubin13/Desktop/Project/uniApp/uni_shop/main.js?{"page":"components%2Fnews-item%2Fnews-item"} ***!
+  \*****************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
+var _newsItem = _interopRequireDefault(__webpack_require__(/*! ./components/news-item/news-item.vue */ 92));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+createPage(_newsItem.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 92:
+/*!**********************************************************************************************!*\
+  !*** /Users/huangzubin13/Desktop/Project/uniApp/uni_shop/components/news-item/news-item.vue ***!
+  \**********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open '/Users/huangzubin13/Desktop/Project/uniApp/uni_shop/components/news-item/news-item.vue'");
+
+/***/ })
+
+},[[91,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/components/news-item/news-item.js.map
---
+++ unpackage/dist/dev/mp-weixin/pages/newsDetail/newsDetail.js
@@ -0,0 +1,31 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/newsDetail/newsDetail"],{
+
+/***/ 88:
+/*!**************************************************************************************************************!*\
+  !*** /Users/huangzubin13/Desktop/Project/uniApp/uni_shop/main.js?{"page":"pages%2FnewsDetail%2FnewsDetail"} ***!
+  \**************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
+var _newsDetail = _interopRequireDefault(__webpack_require__(/*! ./pages/newsDetail/newsDetail.vue */ 89));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+createPage(_newsDetail.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 89:
+/*!*******************************************************************************************!*\
+  !*** /Users/huangzubin13/Desktop/Project/uniApp/uni_shop/pages/newsDetail/newsDetail.vue ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open '/Users/huangzubin13/Desktop/Project/uniApp/uni_shop/pages/newsDetail/newsDetail.vue'");
+
+/***/ })
+
+},[[88,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/newsDetail/newsDetail.js.map
